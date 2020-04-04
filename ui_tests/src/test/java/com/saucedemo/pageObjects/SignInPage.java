d83dfd96ff1551442f3f95412089aeea95c62066
Ozoda Aripova <ozodaxon@gmail.com>
2020-04-04 15:43:50 -0400

    Merge branch 'master' into OzodaAripova
    # Conflicts:
    #	ui_tests/src/test/resources/com/saucedemo/account/signInNegative.feature

--- ui_tests/src/test/java/com/saucedemo/pageObjects/SignInPage.java
+++ ui_tests/src/test/java/com/saucedemo/pageObjects/SignInPage.java
@@ -37,7 +37,7 @@ public class SignInPage extends ModernBasePage {
     }
 
     @And("^I set '(.*)' as password on Sign in page$")
-    public void problem_usersetPassword(String password) {
+    public void setPassword(String password) {
         sendKeys(PASSWORD_INPUT, password);
     }
 
