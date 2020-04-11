c3977b1452becce287ebea9c2b6514811535f433
MAKELAQA <odavic.mladen@gmail.com>
2020-04-11 13:09:00 -0700

    fixed brokenTests.feature

--- java_playground/src/test/java/com/example/ThisIsMyMainClass.java
+++ java_playground/src/test/java/com/example/ThisIsMyMainClass.java
@@ -4,5 +4,8 @@ public class ThisIsMyMainClass {
     public static void main(String[] args) {
 //        Your code goes here, e.g.:
         System.out.println("Hello TojTech World!");
+
+
+
     }
 }
