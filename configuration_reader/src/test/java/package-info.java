1d77f930defe769ed372e60d58eafad3c0325cae
Doniyor Sadullaev <61847018+doniyorbobo@users.noreply.github.com>
2020-04-12 23:25:56 -0400

    Product.feature Sign in

---
+++ configuration_reader/src/test/java/package-info.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Solution {
+    public static int number = 25;
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println(number*number);
+    }
+}
