801472fa7135a427d75317da3019df80a5c5cd29
Ozoda Aripova <ozodaxon@gmail.com>
2020-04-15 21:37:46 -0400

    java.class

---
+++ java_playground/src/test/java/com/example/ThisIsMyClassFromScratch.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class ThisIsMyClassFromScratch {
+    public static void main ( String [] args ){
+     // String, which store multiple characters using double quotes.
+        String name = "Aripova Ozoda";
+     // int, which store whole numbers
+       int age = 35;
+     //char, which store single characters using single quotes.
+        char gender = 'F';
+     // double, which store bigger whole numbers and decimal numbers
+        double desiredSalary = 100.000;
+     //boolean, which store true and false
+        boolean lookingForJob = false;
+
+     // Static typing, which is one of the safety features of Java
+
+
+
+    }
+}
--- java_playground/src/test/java/com/example/ThisIsMyMainClass.java
+++ java_playground/src/test/java/com/example/ThisIsMyMainClass.java
@@ -4,5 +4,15 @@ public class ThisIsMyMainClass {
     public static void main(String[] args) {
 //        Your code goes here, e.g.:
         System.out.println("Hello TojTech World!");
+        /* also code can go here
+        and here
+        this is comment line
+        double - numbers with decimals
+        int - any type of numbers (-+)
+        boolean - true, false
+         */
+
+
+
     }
 }
