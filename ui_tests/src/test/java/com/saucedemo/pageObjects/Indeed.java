ee47acf0c97e72b0b0252915f5d457faf87d347a
Mukhamad Shamsudinov <shamsudinovmukhamad@gmail.com>
2020-04-18 17:51:59 -0500

    job searcher

--- ui_tests/src/test/java/com/saucedemo/pageObjects/Indeed.java
+++ ui_tests/src/test/java/com/saucedemo/pageObjects/Indeed.java
@@ -10,10 +10,13 @@ public class Indeed {
        WebDriver driver = new ChromeDriver();
        //Visiting indeed website
        driver.get("https://www.indeed.com/");
+       //searching for qa jobs
        driver.findElement(By.xpath("//input")).click();
        driver.findElement(By.xpath("//input")).sendKeys("Qa");
        driver.findElement(By.xpath("//button")).click();
-       driver.findElement(By.id("sja1")).click();
+       //clicking on job adv on 1 page
+       driver.findElement(By.id("sja0")).click();
+       //clicking on apply now button
        WebElement applyButton = driver.findElement(By.xpath("//a[.='Apply Now']"));
        WebElement applyOnWebsite = driver.findElement(By.xpath("//a[.='Apply On Company Site']"));
        applyButton.click();
