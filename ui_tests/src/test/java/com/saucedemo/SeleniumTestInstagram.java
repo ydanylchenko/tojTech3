3d66795baacee1f4a474dbc8a34c1c89e38381ab
masrur18 <masrur.sattorov@gmail.com>
2020-04-22 13:24:31 -0400

    Test Instagram by Xpath

--- ui_tests/src/test/java/com/saucedemo/SeleniumTestInstagram.java
+++ ui_tests/src/test/java/com/saucedemo/SeleniumTestInstagram.java
@@ -19,33 +19,22 @@ public class SeleniumTestInstagram {
         driver.findElement(By.linkText("Sign up")).click();
         Thread.sleep(2000);
 //        Type “test@example.com” text into Mobile Number or Email input
-        driver.findElement(By.name("Mobile Number or Email")).sendKeys("test@example.com");
+        driver.findElement(By.xpath("//*[@id=\"react-root\"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input")).sendKeys("test@example.com");
         Thread.sleep(2000);
 //        Type “FirstName LastName” text into Full Name input
-        driver.findElement(By.name("Full Name")).sendKeys("Masrur Sattorov");
+        driver.findElement(By.xpath("//*[@id=\"react-root\"]/section/main/div/article/div/div[1]/div/form/div[4]/div/label/input")).sendKeys("Masrur Sattorov");
         Thread.sleep(2000);
 //        Type “someUsername” text into Username input
-        driver.findElement(By.name("username")).sendKeys("someUsername");
+        driver.findElement(By.xpath("//*[@id=\"react-root\"]/section/main/div/article/div/div[1]/div/form/div[5]/div/label/input")).sendKeys("someUsername");
         Thread.sleep(2000);
 //        Type “P@ssw0rd” text into Password input
-        driver.findElement(By.name("password")).sendKeys("P@ssw0rd");
+        driver.findElement(By.xpath("//*[@id=\"react-root\"]/section/main/div/article/div/div[1]/div/form/div[6]/div/label/input")).sendKeys("P@ssw0rd");
         Thread.sleep(2000);
 //        Click on “Sign up” button
-        driver.findElement(By.linkText("Sign up")).click();
+        driver.findElement(By.xpath("//*[@id=\"react-root\"]/section/main/div/article/div/div[1]/div/form/div[7]/div")).click();
         Thread.sleep(3000);
-
-
-
-
-
-
-
-
-
-
-
-
-
+ //       To close browser
+        driver.quit();
 
     }
 }
