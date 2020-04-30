5fd91ddeaac320e05a8892253ef117af6a6c4463
Yaroslav Danylchenko <ydanylchenko@theladders.com>
2020-04-29 21:11:03 -0400

    Created basic signIn.feature. Created page objects with blank step def

---
+++ jenkins_ui_tests/src/test/java/com/jenkins/pageObjects/DashboardPage.java
@@ -0,0 +1,14 @@
+package com.jenkins.pageObjects;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class DashboardPage {
+    @And("I click log in link on Dashboard page")
+    public void iClickLogInLinkOnDashboardPage() {
+    }
+
+    @Then("I am signed in as THIS user on Dashboard page")
+    public void iAmSignedInAsTHISUserOnDashboardPage() {
+    }
+}
