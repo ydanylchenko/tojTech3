7301d97b12cd61d68cdbe7aa835794bcee87e67b
Firdavs11 <f.nazarovich@gmail.com>
2020-05-08 19:46:29 -0400

    SeleniumTestInstagram

---
+++ java_playground/src/test/java/com/example/ThisIsMyMainClass.java
@@ -0,0 +1,11 @@
+package com.example;
+
+class Operator {
+    public static void main(String[] args){
+     String country = "tajkistan";
+     String word = country.toUpperCase();
+        System.out.println(word);
+        System.out.println("He said \"Hello world\"");
+        System.out.println("Books\nMagazines");
+    }
+}
